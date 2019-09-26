12872503f5b2a1b90d6fc74d32c332f1d9ccfde8
Louis Kueh <louis.kueh@arm.com>
2019-09-26 12:15:18 +0100

    Updated

--- src/DisplayList.js
+++ src/DisplayList.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import axios from 'axios';
-import { ListGroup } from 'react-bootstrap'
+import { ListGroup, Image } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 export default class DisplayList extends React.Component {
 
@@ -26,7 +26,7 @@ export default class DisplayList extends React.Component {
         {this.props.data.map((x, i) =>
           (
             <Link to={"/details/" + x.imdbID}>
-              <ListGroup.Item key={i} >{x.Title}</ListGroup.Item>
+              <ListGroup.Item key={i} >{x.Title}  <Image  style={{width: 50, height: 'auto'}} src={x.Poster}  thumbnail/></ListGroup.Item>
               {/* <ListGroup.Item key={i} >{x.imdbID}</ListGroup.Item> */}
             </Link>
           )
